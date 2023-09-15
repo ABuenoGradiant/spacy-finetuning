832d6fd85f86c903d40af60d7371d7b8e205dd63
ABuenoGradiant <abueno@gradiant.org>
2023-09-15 10:29:34 +0000

    add data span processing to remove nested entities that appears in brands 15092023 corpus

--- generate_spacy_training.py
+++ generate_spacy_training.py
@@ -1,8 +1,9 @@
 # argsparse from https://gist.github.com/ahogen/6fc1760bbf924f4ee6857a08e4fea80a
 import sys,os,json
 import argparse
-from utils import config_file,CURRENT_PATH,read_jsonl,save_jsonl,save_json
+from tqdm import tqdm
 
+from utils import config_file,CURRENT_PATH,read_jsonl,save_jsonl,save_json
 from src.SpacyParser import SpacyParser
 
 CONFIG=config_file()
@@ -32,7 +33,30 @@ def cmdline_args():
 
 
 
+def check_spans(data:list):
+    for entry in data:
+        labels=entry[1]
+        
+        for label in labels:
+            nested_labels=[ l for l in labels if label!=l and l[0]>=label[0] and l[1]<=label[1]]
+            if any(nested_labels):
 
+                if len(nested_labels)==1:
+                    nested_label=nested_labels[0]
+                    if  nested_label[1]-nested_label[0]==1:
+                        if nested_label in entry[1]:
+                            entry[1].remove(nested_label)
+                    else:    
+                        print(nested_labels,entry[0][nested_label[0]:nested_label[1]])
+                        if nested_label[2]=="MISC" and label[2]!="MISC":
+                            entry[1].remove(nested_label)
+                            print(nested_label, "removed!")
+                else:
+                    print(">",label,entry[0][label[0]:label[1]])
+                    print(nested_labels)
+                
+                
+    return data
 
 
 
@@ -60,13 +84,14 @@ if __name__ == '__main__':
             
             paths={}
             training_stats={}
-            for file in os.listdir(data_path):
-                if file in ["train.json","dev.json","test.json"]:
+            for file in tqdm(os.listdir(data_path)):
+                if file.startswith(("train","dev","test")):
                     
                     name=file.replace(".json","")
                     spacy_file="{}.spacy".format(name)
+
                     json_out=read_jsonl(os.path.join(data_path,file))
-                    
+                    # json_out=check_spans(json_out)
                     # generate spacy bin files
                     spacy_db=spacy_parser.to_spacy_format(json_out)
                     spacy_db.to_disk(os.path.join(spacy_bins_path,spacy_file)) 
@@ -75,11 +100,13 @@ if __name__ == '__main__':
                     save_jsonl(os.path.join(spacy_files_path,file),out_spacy)
                     paths[name]=os.path.join(spacy_bins_path,"{}.spacy".format(name))
                     
+                    #training stats process
                     training_stats[file.split(".")[0]]={}
                     original_labels=[ent[2] for entry in json_out for ent in entry[1]]
                     labels=list(set(original_labels))
                     original_labels_count={label:original_labels.count(label) for label in labels}
                     training_stats[file.split(".")[0]]["original"]=dict(original_labels_count,**{"len":len(json_out)})
+                    
                     spacy_labels=[ent["label"] for entry in out_spacy for ent in entry["spans"]]
                     labels=list(set(spacy_labels))
                     spacy_labels_count={label:spacy_labels.count(label) for label in labels}
@@ -100,6 +127,16 @@ if __name__ == '__main__':
                     os.path.join(out_path,"model-best"), paths["test"],os.path.join(out_path,"evaluation.json"),os.path.join(out_path,"evaluation.txt")
                 ))
                 bash_out.write("\n")
+                
+                for test_name in paths.keys():
+                    if test_name.startswith("test_"):
+                        lang=test_name.split("_")[-1]
+                        bash_out.write(
+                            "python3 -m spacy benchmark accuracy {} {} -o {}".format(
+                            os.path.join(out_path,"model-best"), paths[test_name],os.path.join(out_path,"evaluation_{}.json".format(lang))
+                            ))
+                        bash_out.write("\n")
+                
                 bash_out.write("python3 {} {}".format(os.path.join(CURRENT_PATH,"to_ml_flow.py"),training_name))
             
             print("Done! please run: \nbash {}\n to train the model.".format(os.path.join(out_path,"run_train.sh")))
